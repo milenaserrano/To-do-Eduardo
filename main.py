03df3a86e5618eadb55325226f49a39846ee4386
Milena <68025219+milenaserrano@users.noreply.github.com>
2022-03-24 23:58:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+app = Flask('app')
+
+todos = [
+    { 
+        'name': 'Estudar Python',
+        'complete': False
+    },
+    {
+        'name': 'Estudar JavaScript',
+        'complete': False
+    },
+    {
+        'name': 'Fazer exercício 1',
+        'complete': True
+    }
+]
+
+@app.route('/')
+def index():
+    return render_template(
+        'index.html',
+        todos=todos
+    )
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
